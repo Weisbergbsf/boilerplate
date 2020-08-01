dcf2fc8f5f4e94ed11a4ce43573cbddc818e1e40
Barros Siqueira Fontes <wbarros@indracompany.com>
2020-08-01 11:13:28 -0300

    Add support to husky and lint-staged

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../../styles/Home.module.css'
 
-export default function Home() {
+const foo = 'blabalbla'
+
+export default function Home({ title = 'React avançado' }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -10,9 +12,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>{title}</h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
